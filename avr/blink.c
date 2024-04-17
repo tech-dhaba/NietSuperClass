3f0b836ab9f2dbd3fed8747c7723375c8ea0c9b0
ShivamBhadani <sbhadani882@outlook.com>
2024-04-17 09:40:26 +0530

    added folder avr and .gitignore

---
+++ avr/blink.c
@@ -0,0 +1,14 @@
+#include<avr/io.h>
+#include<util/delay.h>
+
+int main(void)
+{
+    DDRB=DDRB|(1<<DDB5);
+    while(1)
+    {
+        PORTB = PORTB | (1<<PORTB5);
+        _delay_ms(50);
+        PORTB = PORTB & ~(1<<PORTB5);
+        _delay_ms(500);
+    }
+}
