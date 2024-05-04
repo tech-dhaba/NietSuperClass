8ce5fe9748e73d8a923db01cf6908050acc9cc99
ShivamBhadani <sbhadani882@outlook.com>
2024-05-04 16:31:11 +0530

    updated makefile

--- avr/blink.c
+++ avr/blink.c
@@ -1,14 +1,24 @@
 #include<avr/io.h>
 #include<util/delay.h>
+#include<stdint.h>
 
 int main(void)
 {
     DDRB=DDRB|(1<<DDB5);
+     DDRB=DDRB&~(1<<DDB6);
+
     while(1)
     {
-        PORTB = PORTB | (1<<PORTB5);
-        _delay_ms(50);
-        PORTB = PORTB & ~(1<<PORTB5);
-        _delay_ms(200);
+        volatile uint8_t a=(PINB & (1<<PINB6)) ? 1 : 0;
+        if(a){
+                    PORTB = PORTB | (1<<PORTB5);
+        }
+        else{
+                    PORTB = PORTB & ~(1<<PORTB5);
+        }
+        // PORTB = PORTB | (1<<PORTB5);
+        // _delay_ms(50);
+        // PORTB = PORTB & ~(1<<PORTB5);
+        // _delay_ms(200);
     }
 }
